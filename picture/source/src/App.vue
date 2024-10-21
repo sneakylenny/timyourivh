7753827883610ffb4953b594de136b14ae38db4e
Tim van Herwijnen <t.vanherwijnen@helmink.nl>
2024-10-21 17:45:48 +0200

    Add profile picture

---
+++ picture/source/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import SneakyLenny from './components/SneakyLenny.vue';
+</script>
+
+<template>
+  <div class="container">
+    <SneakyLenny />
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: max-content;
+  border: dashed red;
+}
+</style>
---
+++ picture/source/src/components/SneakyLenny.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <div class="background">
+    <div class="container">
+      <span class="lennyface">
+        ( ͡° ͜ʖ ͡°)
+      </span>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.background {
+  background: white;
+  width: 48rem;
+  height: 48rem;
+}
+
+.container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: conic-gradient(
+    hsl(0, 100%, 50%) 0deg,
+    hsl(60, 100%, 50%) 60deg,
+    hsl(120, 100%, 50%) 120deg,
+    hsl(180, 100%, 50%) 180deg,
+    hsl(240, 100%, 50%) 240deg,
+    hsl(300, 100%, 50%) 300deg,
+    hsl(0, 100%, 50%) 360deg
+  );
+  color: transparent;
+  background-clip: text;
+}
+
+.lennyface {
+  font-family: Arial, sans-serif;
+  font-size: 16em;
+  transform: scaleY(1.2) translateY(-6%);
+}
+</style>
